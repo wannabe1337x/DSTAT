7db33559cceac68f8917371fd33fd7f401917b94
Wannabe1337 <1337.wannabe404@gmail.com>
2025-01-31 14:24:35 +0100

    index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-const PORT = process.env.PORT || 8082;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
